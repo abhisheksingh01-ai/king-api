e57e176bfe723e61204ea00c06c13731c2437cc2
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-01-12 17:24:54 +0530

    done

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 // ✅ FIXED CORS (cookies + frontend)
 app.use(
   cors({
-    origin: "https://king-frontend-mu.vercel.app/",
+    origin: "https://king-frontend-mu.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
